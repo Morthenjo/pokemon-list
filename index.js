e13fc970dcd90142ac5c8d10fb3f57825fe5f2c0
MorthenJo <MorthenJo.kodehode@gmail.com>
2022-10-31 13:19:21 +0100

    test

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function submit(e) {
 }
 
 // button2.addEventListener("click", submit);
-button2.addEventListener("mousedown", submit);
+button2.addEventListener("touch", submit);
 
 function renderPokemon(data) {
   const li = document.createElement("li");
